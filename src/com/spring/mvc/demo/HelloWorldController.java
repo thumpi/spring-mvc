4394552b464dfe252aa0eacb51965f8b6867c12d
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2020-03-29 20:56:32 +0530

    getting html data directly using @requestparam

--- src/com/spring/mvc/demo/HelloWorldController.java
+++ src/com/spring/mvc/demo/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.HttpServletBean;
 
 @Controller
@@ -15,14 +16,13 @@ public class HelloWorldController {
 	 return "helloworld-form";
 	}	
 	
-	@RequestMapping("/processForm")
-	public String filledForm() {
-	 return "helloworld";
-	}
+//	@RequestMapping("/processForm")
+//	public String filledForm() {
+//	 return "helloworld";
+//	}
 	
 	@RequestMapping("/processFormVersionTwo")
-	public String getdataToUppercase(HttpServletRequest request, Model model) {
-		String theName=request.getParameter("studentName");
+	public String getdataToUppercase(@RequestParam("studentName") String theName, Model model) {
 		theName=theName.toUpperCase();
 		String message= "Hello " + theName;
 		model.addAttribute("studentName", message);
